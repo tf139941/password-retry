68ddc8fe6957a1a50d7506208f88d39899347039
Guanlin <tf139941@gmail.com>
2021-04-07 20:26:52 +0800

    add print

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,14 @@
 x = 3
-while x <= 3 and x > 0:
+while  x > 0:
+	x = x - 1
 	passwprd = input('請輸入密碼:')
 	if passwprd == 'a123456':
 		print('登入成功')
 		break
 	else:
-		x = x - 1
-		if x == 0:
-			break
 		print('密碼錯誤還有', x, '次機會')
+		if x == 0:
+			print('密碼錯誤次數過多帳號已被鎖定!!!')
+			break
+		
 
